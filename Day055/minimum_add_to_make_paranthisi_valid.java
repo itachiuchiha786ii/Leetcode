d0ef43bf20090d9200a4d8d1a07fa5709ca83f4b
itachiuchiha786ii <leeveronica979@gmail.com>
2026-01-10 22:55:16 +0530

    leetcode probelm 921

---
+++ Day055/minimum_add_to_make_paranthisi_valid.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int add=0;
+		int open=0;
+		for(int i=0;i<s.length();i++){
+			if(s.charAt(i)== '('){
+				open++;
+			}else if(s.charAt(i)==')'){
+				if(open>0){
+					open--;
+				}else{
+				add++;
+				}
+			}
+        }
+        return open+add;
+    }
+}
