3cc816cc7a99de48fc50e0eb2b2f49cdd39e9178
itachiuchiha786ii <leeveronica979@gmail.com>
2026-01-23 00:48:57 +0530

    leetcode problem 328

---
+++ Day066/even_odd_linkedlist.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null || head.next.next == null){
+        	return head;
+        }
+        ListNode oddh=head;
+        ListNode evenh=head.next;
+        ListNode evn=evenh;
+        while(evenh != null && evenh.next != null){
+        	oddh.next=oddh.next.next;
+        	evenh.next=evenh.next.next;
+        	
+        	oddh=oddh.next;
+        	evenh=evenh.next;
+        }
+        oddh.next=evn;
+        return head;
+    }
+}
