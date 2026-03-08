167900f5bb2376c7ede4dce2f6d19440fedbc05a
itachiuchiha786ii <leeveronica979@gmail.com>
2026-03-08 16:56:15 +0530

    shame on me feel guilt

---
+++ Day109/convrt_binary_into_num_in_a_linkedlist.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        if(head==null){
+        	return 0;
+        }else if(head.next==null){
+        	return head.val;
+        }
+       int num=0;
+        while(head!=null){
+            num=num*2+head.val;
+            head=head.next;
+        }
+        return num;
+    }
+}
